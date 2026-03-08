417f1b9714a24d50e9ffaf71e6af39643ce67555
Geoffroy Vallee <geoffroy.vallee@gmail.com>
2026-03-08 11:26:29 -0700

    chore(license): update NVIDIA header in results
    Update the existing NVIDIA copyright line to 2021-2026.\nFix license filename reference from LICENSE.md to LICENSE.\nNo code logic or API changes.

--- pkg/results/results.go
+++ pkg/results/results.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2019, Sylabs Inc. All rights reserved.
-// Copyright (c) 2021, NVIDIA CORPORATION. All rights reserved.
+// Copyright (c) 2021-2026, NVIDIA CORPORATION. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
-// LICENSE.md file distributed with the sources of this project regarding your
+// LICENSE file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
 package results
